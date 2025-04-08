8bf8e240cb27c6f8710d7335886923b0a77e519c
thoon <winwin_th@naver.com>
2025-04-08 18:09:36 +0900

    Style: 응답 키 수정 (data -> result)

--- src/main/kotlin/team/goodpeople/global/response/ApiResponse.kt
+++ src/main/kotlin/team/goodpeople/global/response/ApiResponse.kt
@@ -3,11 +3,11 @@ package team.goodpeople.global.response
 data class ApiResponse<T>(
     val status: Int,
     val message: String,
-    val data: T? = null
+    val result: T? = null
 ) {
     companion object {
-        fun <T> success(data: T, status: Int = 200, message: String = "request success"): ApiResponse<T> =
-            ApiResponse(data = data, status = status, message = message)
+        fun <T> success(result: T, status: Int = 200, message: String = "request success"): ApiResponse<T> =
+            ApiResponse(result = result, status = status, message = message)
 
         fun <T> failure(status: Int = 400, message: String = "request failed"): ApiResponse<T> =
             ApiResponse(status = status, message = message)
