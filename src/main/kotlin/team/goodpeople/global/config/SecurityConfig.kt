7a9c55f8115dfebb52f897b87acb76ac038dd84b
thoon <winwin_th@naver.com>
2025-04-11 11:50:46 +0900

    Feat: SecurityConfig 기본 작성

---
+++ src/main/kotlin/team/goodpeople/global/config/SecurityConfig.kt
@@ -0,0 +1,32 @@
+package team.goodpeople.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+@EnableWebSecurity
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .formLogin { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+
+        http
+            .authorizeHttpRequests {
+                it
+                    .requestMatchers("/", "/login").permitAll()
+                    .requestMatchers("/admin/**").hasRole("ADMIN")
+                    .anyRequest().permitAll()
+            }
+
+        return http.build()
+    }
+
+}
