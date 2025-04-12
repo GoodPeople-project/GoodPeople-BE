1e5b91cff54177b7795b171df5dd14a929751172
thoon <winwin_th@naver.com>
2025-04-12 14:30:13 +0900

    Chore: JWT 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,6 +46,11 @@ dependencies {
 
     // Security
     implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
 }
 
 kotlin {
