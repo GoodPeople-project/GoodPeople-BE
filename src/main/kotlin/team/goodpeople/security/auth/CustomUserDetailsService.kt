0cbff83339c16c72cb6f9cf154ba47800d773d34
thoon <winwin_th@naver.com>
2025-04-12 20:31:58 +0900

    Refactor: User가 조회되지 않을 경우, 예외를 던지도록 로직 변경

--- src/main/kotlin/team/goodpeople/security/auth/CustomUserDetailsService.kt
+++ src/main/kotlin/team/goodpeople/security/auth/CustomUserDetailsService.kt
@@ -2,6 +2,7 @@ package team.goodpeople.security.auth
 
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
 import team.goodpeople.user.repository.UserRepository
 
@@ -10,14 +11,10 @@ class CustomUserDetailsService(
     private val userRepository: UserRepository
 ) : UserDetailsService {
 
-    override fun loadUserByUsername(username: String): UserDetails? {
+    override fun loadUserByUsername(username: String): UserDetails {
 
-        val user = userRepository.findUserByUsername(username)
+        val user = userRepository.findUserByUsername(username) ?: throw UsernameNotFoundException("사용자를 찾을 수 없습니다.")
 
-        if (user != null) {
-            return CustomUserDetails(user)
-        }
-
-        return null
+        return CustomUserDetails(user)
     }
 }
