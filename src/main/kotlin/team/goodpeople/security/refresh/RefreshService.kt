e51d0c27dd1488bf125777b403bc5e548c4a83d8
thoon <winwin_th@naver.com>
2025-04-14 00:54:33 +0900

    Feat: Refresh Token의 CRD에 사용할 서비스 클래스. RedisTemplate을 주입 받아 사용

--- src/main/kotlin/team/goodpeople/security/refresh/RefreshService.kt
+++ src/main/kotlin/team/goodpeople/security/refresh/RefreshService.kt
@@ -1,23 +1,58 @@
 package team.goodpeople.security.refresh
 
-class RefreshService {
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.stereotype.Service
+import team.goodpeople.security.jwt.JWTConstants.REFRESH_EXPIRED_MS
+import java.time.Duration
 
+@Service
+class RefreshService(
+    @Qualifier("redisRefreshTemplate")
+    private val redisTemplate: RedisTemplate<String, String>
+) {
     /**
      * Redis에 저장될 Refresh Token
      * - 저장 기간은 JWT Constants를 참조
-     * - TODO
-     * Key는 Refresh Token 자체 또는 사용자 id 등과 조합하여 저장.
-     * 혹은 Refresh Token을 Key, 사용자 id 등을 Value로 해서 검증해도 될 것 같다.
-     * 반대로도 가능할 듯
+     * - TODO: 저장 형식
+     * 현재 사용자 인증 시에는 username을 사용하므로, prefix + username 형식으로 키를 설정했다.
+     *
+     * 사용자 id 등을 Value로 해서 검증해도 될 것 같다.
+     * 더 나은 방식을 고민해볼 것
      * */
+    // Key에 사용할 prefix
+    private val prefix = "refresh_token:"
 
-    // TODO: Redis에 Refresh Token 저장
+    // Refresh Token 저장
+    fun saveRefreshToken(
+        username: String,
+        refreshToken: String,
+    ) {
+        val key = prefix + username
 
+        redisTemplate.opsForValue()
+            .set(key, refreshToken)
 
-    // TODO: Redis에 저장된 Refresh Token 조회
+        redisTemplate.expire(key, Duration.ofMillis(REFRESH_EXPIRED_MS))
+    }
 
+    // 저장된 Refresh Token 조회
+    // 없을 경우 빈 문자열을 반환
+    fun getRefreshToken(
+        username: String
+    ): String {
+        val key = prefix + username
 
-    // TODO: Redis에 저장된 Refresh Token 삭제
+        return redisTemplate.opsForValue().get(key) ?: ""
+    }
 
+    // 저장된 Refresh Token 삭제
+    fun deleteRefreshToken(
+        username: String
+    ) {
+       val key = prefix + username
+
+       redisTemplate.delete(key)
+    }
 
 }
