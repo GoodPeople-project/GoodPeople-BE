49b8ebcf06ceff63cdc110092997d34f5fd42808
thoon <winwin_th@naver.com>
2025-04-14 00:50:32 +0900

    Feat: RedisConfig 클래스 작성

--- src/main/kotlin/team/goodpeople/global/config/RedisConfig.kt
+++ src/main/kotlin/team/goodpeople/global/config/RedisConfig.kt
@@ -1,5 +1,43 @@
 package team.goodpeople.global.config
 
-// TODO: Reids 연동
-class RedisConfig {
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@EnableRedisRepositories
+@Configuration
+class RedisConfig(
+    @Value("\${spring.data.redis.host}") private val hostname: String,
+    @Value("\${spring.data.redis.port}") private val port: Int,
+    @Value("\${spring.data.redis.password}") private val password: String,
+) {
+
+    @Bean
+    fun redisConnectionFactory(): RedisConnectionFactory {
+        val configuration = RedisStandaloneConfiguration(hostname, port)
+        configuration.setPassword(password)
+
+        return LettuceConnectionFactory(configuration)
+    }
+
+    @Bean
+    fun redisRefreshTemplate(): RedisTemplate<String, String> {
+        val refreshTemplate = RedisTemplate<String, String>()
+
+        refreshTemplate.apply {
+            connectionFactory = redisConnectionFactory()
+            keySerializer = StringRedisSerializer()
+            valueSerializer = StringRedisSerializer()
+            hashKeySerializer = StringRedisSerializer()
+            hashValueSerializer = StringRedisSerializer()
+        }
+
+        return refreshTemplate
+    }
 }
