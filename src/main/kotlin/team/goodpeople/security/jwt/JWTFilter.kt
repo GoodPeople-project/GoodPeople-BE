6cc05faa42f5d8aef388a83842809b6eee067e8f
thoon <winwin_th@naver.com>
2025-04-15 16:19:02 +0900

    Feat: JWTFilter 작성. 예외처리 필요

--- src/main/kotlin/team/goodpeople/security/jwt/JWTFilter.kt
+++ src/main/kotlin/team/goodpeople/security/jwt/JWTFilter.kt
@@ -1,26 +1,80 @@
 package team.goodpeople.security.jwt
 
-class JWTFilter {
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.JwtException
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.filter.OncePerRequestFilter
+import team.goodpeople.security.auth.CustomUserDetails
+import team.goodpeople.user.entity.Role
+import team.goodpeople.user.entity.User
 
-    /** Access Token 검증 로직 */
+class JWTFilter(
+    private val jwtUtil: JWTUtil,
+) : OncePerRequestFilter() {
 
-    // TODO: Request에서 Authorization 헤더 추출
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        /**
+         * Access Token 검증 로직
+         * - 실패 시 early return 후, 다음 필터로 넘어간다.
+         * */
 
-    // TODO: JWT Constants의 TYPE과 일치하는가. 일치하면 Access Token 문자열만 추출
+        /**
+         * resolveAccessToken 메서드
+         * - 헤더 검증 및 Access Token을 추출한다.
+         * - null이면
+         * 1. 인증 헤더를 가져온다.
+         * 2. Prefix가 TOKEN_TYPE과 일치하는지 확인한다.
+         * 3. Prefix를 제거하여 순수한 토큰 문자열을 획득한다.
+         * */
+        try {
+            val accessToken: String = jwtUtil.resolveAccessToken(request)
+                ?: return filterChain.doFilter(request, response)
 
-    // TODO: cat이 Access Token을 나타내는지 확인
+            /** cat이 Access Token을 나타내는지 확인 */
+            if (!jwtUtil.isAccessToken(accessToken)) {
+                return filterChain.doFilter(request, response)
+            }
 
-    // TODO: 만료 여부 확인
+            /** 토큰 만료 여부 확인 */
+            if (!jwtUtil.isTokenExpired(accessToken)) {
+                return filterChain.doFilter(request, response)
+            }
 
-    /** 검증 후 Context에 사용자 등록 */
+            /**
+             * 검증 후 Context에 사용자 등록
+             * 팩토리 메서드로 user 객체를 생성하여 Details에 넘긴다.
+             * */
+            val username = jwtUtil.getClaim(accessToken, "username")
+            val role = jwtUtil.getClaim(accessToken, "role")
+            val userId = jwtUtil.getClaim(accessToken, "userId").toLong()
 
-    // TODO: Access Token에서 사용자 정보 추출
+            val user = User.createEntityForJWT(
+                id = userId,
+                username = username,
+                role = Role.valueOf(role))
+            val userDetails = CustomUserDetails(user)
 
-    // TODO: UserDetails 생성
+            val authenticationToken = UsernamePasswordAuthenticationToken(userDetails, null)
 
-    // TODO: Security 토큰 생성
+            /** Security Context Holder에 저장 */
+            SecurityContextHolder.getContext().authentication = authenticationToken
 
-    // TODO: Security Context Holder에 저장
+            filterChain.doFilter(request, response)
+        // TODO: 예외처리 구체화
+        } catch (e: ExpiredJwtException) {
+            // TODO: Response Exception
+        } catch (e: JwtException) {
+            // TODO: Response Exception
+        }
 
-    // TODO: 다음 필터 실행
+
+    }
 }
