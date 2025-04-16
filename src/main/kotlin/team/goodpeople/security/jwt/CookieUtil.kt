aa74f5ee980602ce155cbc8da01b535abe12a84a
thoon <winwin_th@naver.com>
2025-04-16 11:46:49 +0900

    Feat: Refresh Token을 담을 Cookie Util

---
+++ src/main/kotlin/team/goodpeople/security/jwt/CookieUtil.kt
@@ -0,0 +1,57 @@
+package team.goodpeople.security.jwt
+
+import jakarta.servlet.http.Cookie
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import team.goodpeople.security.jwt.JWTConstants.COOKIE_DOMAIN
+import team.goodpeople.security.jwt.JWTConstants.COOKIE_HTTP_ONLY
+import team.goodpeople.security.jwt.JWTConstants.COOKIE_MAX_AGE
+import team.goodpeople.security.jwt.JWTConstants.COOKIE_PATH
+import team.goodpeople.security.jwt.JWTConstants.COOKIE_SET_SECURE
+
+class CookieUtil {
+
+    companion object {
+        fun sendCookie(
+            response: HttpServletResponse,
+            key: String,
+            value: String
+        ) {
+            val cookie = Cookie(key, value)
+
+            cookie.apply {
+                maxAge = COOKIE_MAX_AGE
+                domain = COOKIE_DOMAIN
+                path = COOKIE_PATH
+                secure = COOKIE_SET_SECURE
+                isHttpOnly = COOKIE_HTTP_ONLY
+            }
+
+            response.addCookie(cookie)
+        }
+
+        fun deleteCookie(
+            response: HttpServletResponse,
+            key: String
+        ) {
+            val cookie = Cookie(key, null)
+
+            cookie.apply {
+                maxAge = 0
+                domain = COOKIE_DOMAIN
+                path = COOKIE_PATH
+                secure = COOKIE_SET_SECURE
+                isHttpOnly = COOKIE_HTTP_ONLY
+            }
+
+            response.addCookie(cookie)
+        }
+
+        fun getStringFromCookies(
+            request: HttpServletRequest,
+            key: String
+        ) {
+            request.cookies?.firstOrNull { it.name == key }
+        }
+    }
+}
--- src/main/kotlin/team/goodpeople/security/jwt/JWTConstants.kt
+++ src/main/kotlin/team/goodpeople/security/jwt/JWTConstants.kt
@@ -12,4 +12,13 @@ object JWTConstants {
     /** - Refresh Token Constants */
     const val REFRESH_CATEGORY: String = "refresh"
     const val REFRESH_EXPIRED_MS: Long = 60 * 60 * 1000L // 1시간
+
+
+    /** Cookie Constants */
+    const val COOKIE_DOMAIN: String = "localhost"
+    const val COOKIE_PATH: String = "/"
+    const val COOKIE_MAX_AGE: Int = 24 * 60 * 60 * 1000 // 1일
+
+    const val COOKIE_HTTP_ONLY: Boolean = true
+    const val COOKIE_SET_SECURE: Boolean = true
 }
