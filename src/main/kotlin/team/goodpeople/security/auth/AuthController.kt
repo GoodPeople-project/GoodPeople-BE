8fa0588836afcf2fa0e78d7180f7e3a9fab27a6a
thoon <winwin_th@naver.com>
2025-04-18 18:37:43 +0900

    Feat: 인증 관련 엔드포인트 분기 컨트롤러 AuthController 작성

---
+++ src/main/kotlin/team/goodpeople/security/auth/AuthController.kt
@@ -0,0 +1,39 @@
+package team.goodpeople.security.auth
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import team.goodpeople.global.response.ApiResponse
+import team.goodpeople.security.refresh.RefreshService
+
+@RequestMapping("/api/auth")
+@RestController
+class AuthController(
+    private val refreshService: RefreshService
+) {
+
+    @PostMapping("/refresh")
+    fun reissueAccess(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+    ): ApiResponse<Boolean> {
+        val result = refreshService.reissueAccessToken(request, response)
+
+        return ApiResponse.success(
+            result = result,
+            status = 200,
+            message = "Reissue Successful"
+        )
+    }
+
+    /**
+     * 내부 로직으로 생성되는 엔드포인트
+     * - 로그인
+     * @PostMapping("/login")
+     *
+     * - 로그아웃
+     * @PostMapping("/logout")
+     * */
+}
