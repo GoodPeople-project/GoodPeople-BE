c7f192e4798c8fdeb5ff6b3e4d4afa31ff94f4c5
thoon <winwin_th@naver.com>
2025-04-20 19:40:38 +0900

    Rename: Security 관련 Config 클래스 디렉터리 구조 변경

--- src/main/kotlin/team/goodpeople/security/config/SecurityConfig.kt
+++ src/main/kotlin/team/goodpeople/security/config/SecurityConfig.kt
@@ -1,4 +1,4 @@
-package team.goodpeople.security
+package team.goodpeople.security.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
