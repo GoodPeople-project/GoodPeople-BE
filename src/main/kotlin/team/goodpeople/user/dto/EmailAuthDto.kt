de851ba66880e58ed49e6b74bb5c814cc7091179
thoon <winwin_th@naver.com>
2025-04-24 14:39:14 +0900

    Feat: 이메일 인증 완료 처리

--- src/main/kotlin/team/goodpeople/user/dto/EmailAuthDto.kt
+++ src/main/kotlin/team/goodpeople/user/dto/EmailAuthDto.kt
@@ -4,10 +4,13 @@ import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
 
-data class EmailDto(
+data class EmailAuthDto(
     @field:Size(min = 5, max = 100)
     @field:NotNull(message = "email cannot be blank")
     @field:Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$")
-    val email: String = ""
+    val email: String,
+    @field:Size(min = 6, max = 6)
+    @field:NotNull(message = "auth code cannot be blank")
+    val emailAuthCode: String
 ) {
 }
