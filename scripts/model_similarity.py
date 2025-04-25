be08dbb3dd32d839613ac83582d0c1dc967f67ca
thoon <winwin_th@naver.com>
2025-04-25 16:08:53 +0900

    Refactor: 소요 시간 로그 삭제

--- scripts/model_similarity.py
+++ scripts/model_similarity.py
@@ -3,12 +3,9 @@ import datetime
 import sys
 import io
 import numpy as np
-import pandas as pd
 from sentence_transformers import SentenceTransformer, util
 import time
 import pickle
-
-initial_time = time.time()
 
 def log_time(tag, start):
     end = time.time()
@@ -16,13 +13,9 @@ def log_time(tag, start):
     sys.stdout.flush()
     return end
 
-step_time = log_time("초기", initial_time) # Log
-
 # TODO: 바이너리 데이터로 읽으면 읽기 속도 더 빨라질 수도 있음.
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding="utf-8")
 sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding="utf-8")
-
-step_time = log_time("입출력 설정 후", initial_time) # Log
 
 # # 판례 CSV 데이터 경로
 # path = 'C:\dev\goodpeople\scripts\data\law_data.csv'
@@ -31,11 +24,8 @@ step_time = log_time("입출력 설정 후", initial_time) # Log
 with open('C:/dev/goodpeople/scripts/cache/law_cache.pkl', 'rb') as f:
     df, x = pickle.load(f)
 
-step_time = log_time("CSV 읽기", step_time) # Log
-
 # sentence-transformer 내장 모델
 model = SentenceTransformer('distiluse-base-multilingual-cased-v2')
-step_time = log_time("모델 로딩", step_time) # Log
 
 # 사용자가 입력한 스크립트를 text 변수에 저장, 내용이 없으면 exit()
 if len(sys.argv) < 2:
@@ -44,19 +34,14 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 text = sys.argv[1]
-step_time = log_time("사용자 입력 저장", step_time) # Log
 
 # 입력한 텍스트를 인코딩한다.-> 캐싱으로 패스
 # x = model.encode(df['story'])
-# step_time = log_time("기준 사례 인코딩", step_time) # Log
-
 y = model.encode(text)
-step_time = log_time("사용자 입력 인코딩", step_time) # Log
 
 # 유사도 계산
 cos_scores = util.pytorch_cos_sim(x, y).squeeze()
 cos_scores = np.array(cos_scores)
-step_time = log_time("유사도 계산", step_time) # Log
 
 # 유사한 사례를 3개 반환한다.
 target_index = np.argsort(cos_scores)[::-1][:3]
@@ -74,5 +59,3 @@ for i in range(3):
     print(df['result'][target_index[i]])
     print('')
 
-log_time("전체 소요 시간: ", initial_time) # Log
-print("스크립트 종료 후 시각", datetime.datetime.now()) # Log
