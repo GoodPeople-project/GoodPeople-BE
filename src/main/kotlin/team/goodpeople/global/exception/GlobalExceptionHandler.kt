1649c755b5aac702b5477a522b94d1c2d0d481d8
thoon <winwin_th@naver.com>
2025-05-02 23:19:40 +0900

    Feat: 없는 API 요청 시 404 응답

--- src/main/kotlin/team/goodpeople/global/exception/GlobalExceptionHandler.kt
+++ src/main/kotlin/team/goodpeople/global/exception/GlobalExceptionHandler.kt
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.servlet.NoHandlerFoundException
 import team.goodpeople.global.response.ApiResponse
 
 @RestControllerAdvice
@@ -15,7 +16,20 @@ class GlobalExceptionHandler {
         val error = e.errorCode
         return ResponseEntity
             .status(error.status)
-            .body(ApiResponse.failure(status = error.status.value(), message = error.message))
+            .body(ApiResponse.failure(
+                status = error.status.value(),
+                message = error.message))
+    }
+
+    /* 존재하지 않는 API 요청 시 404 응답 */
+    @ExceptionHandler(NoHandlerFoundException::class)
+    fun noHandlerFoundException(e: NoHandlerFoundException): ResponseEntity<ApiResponse<Nothing>> {
+
+        return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .body(ApiResponse.failure(
+                status = HttpStatus.NOT_FOUND.value(),
+                message = "No Resources Found"))
     }
 
     /* 그 외 예외 응답 */
@@ -24,6 +38,8 @@ class GlobalExceptionHandler {
         val status = HttpStatus.INTERNAL_SERVER_ERROR.value()
         return ResponseEntity
             .status(status)
-            .body(ApiResponse.failure(status = status, message = "오류가 발생했습니다."))
+            .body(ApiResponse.failure(
+                status = status,
+                message = "오류가 발생했습니다."))
     }
 }
